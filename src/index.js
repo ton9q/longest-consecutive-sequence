73244e9635dbe97c7b540da10dd0a074a60b6e69
ton9q <kuchma_anton@mail.ru>
2018-09-17 23:17:38 +0300

    Addiction of logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  if (!array.length) return 0;
+  
+  array = array.sort((a,b) => a-b);
+
+  let counter = 1;
+  let maxCounter = 1;
+
+  for(let i = 1; i < array.length; i++) {
+    if(array[i] === array[i-1] + 1) {
+      counter++; 
+    } else if (array[i] === array[i-1]) {
+      continue;
+    } else{
+      if(counter > maxCounter) maxCounter = counter;
+      counter = 1;
+    }
+  }
+  
+  return maxCounter;
+};
